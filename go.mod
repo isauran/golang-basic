edaf7dfbe3604ac6a40ab2f6454df4d370e44691
Sauranb <Sauranb@freedombank.kz>
2025-01-22 10:22:35 +0500

    utils v2

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/isauran/golang-basic
+module github.com/isauran/golang-basic/v2
 
 go 1.23.2
--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -1,19 +1,19 @@
 package utils
 
-func Contains(a []string, x string) bool {
-	for _, n := range a {  
-		if x == n {     
-		   return true     
-		}  
-	 }  
-	 return false
+func InSlice(a []string, x string) bool {
+	for _, n := range a {
+		if x == n {
+			return true
+		}
+	}
+	return false
 }
 
 func ContainsInt(a []int, x int) bool {
-	for _, n := range a {  
-	   if x == n {     
-		  return true     
-	   }  
-	}  
+	for _, n := range a {
+		if x == n {
+			return true
+		}
+	}
 	return false
-  }
+}
